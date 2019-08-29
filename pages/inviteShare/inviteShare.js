ec0d5ddf5c982e551a10e0f150775d5a369a76c2
consolexuhuang <1758050394@qq.com>
2019-08-29 11:46:28 +0800

    优化代码

--- pages/inviteShare/inviteShare.js
+++ pages/inviteShare/inviteShare.js
@@ -28,18 +28,9 @@ Page({
     copyLinkGZH: getApp().globalData.copyLinkGZH,
     tipsState: false, //提示
     userAdmin:'', //当前用户身份 1：自己，2: 新用户, 3: 其他（老用户下单/为下单）
-    // isReceiveState: false
-
-    // navbarData: {
-    //   title: 'Justin&Julie',
-    //   showCapsule: 0,
-    //   isShowBackHome: false,
-    //   titleColor: "#000",
-    //   tab_topBackground: '#fff'
-    // },
-    // marginTopBar: getApp().globalData.tab_height * 2 + 20
   },
 // ----------------------------------------公共方法层
+  //判断当前用户身份
   getUserAdmin(){
     if (!getApp().globalData.share){
       this.setData({ userAdmin: '' })
@@ -178,8 +169,6 @@ Page({
     // this.getInviteQrcode()
   },
 // -----------------------------------------------逻辑渲染层
-  onReady(){
-  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -222,6 +211,7 @@ Page({
     // this.setData({ isShare : getApp().globalData.share })
   },
 // -----------------------------------------------视图层事件
+  // 授权method
   bindgetuserinfo(){
     getApp().wx_loginIn().then(() => {
       wx.setNavigationBarColor({
@@ -258,20 +248,7 @@ Page({
       }
     })
   },
-  // 长摁识别
-  // distinguishImg() {
-  //   this.setData({
-  //     showShareState: true
-  //   }, () => {
-  //     this.showCanvas(this.data.postConfig.shareBgImg, this.data.postConfig.codeImg)
-  //   })
-  // },
-  // 关闭分享
-  // onCloseShareImg() {
-  //   this.setData({
-  //     showShareState: false
-  //   })
-  // },
+  
   // 显示面对面
   handleFaceInviteBtnTap: function (event) {
     const isShadeShow = true
@@ -279,12 +256,6 @@ Page({
       isShadeShow
     })
   },
-  // //关闭海报
-  // onCloseShareImg() {
-  //   this.setData({
-  //     showShareState: false
-  //   })
-  // },
   //关闭面对面
   onClose(){
     this.setData({
@@ -317,6 +288,7 @@ ${this.data.copyLinkGZH}`,
       }
     })
   },
+  //jump规则页
   jumpToRuleDetail(){
     wx.navigateTo({
       url: '/pages/ruleDetail/ruleDetail',
@@ -339,6 +311,7 @@ ${this.data.copyLinkGZH}`,
       }
     }
   },
+  //下拉
   onPullDownRefresh(){
     if (this.data.IsshowNetStatus){
       if (!Store.getItem('userData')) {
@@ -349,173 +322,196 @@ ${this.data.copyLinkGZH}`,
         this.LoadPageFunc()
       }
     }
-    
     wx.stopPullDownRefresh()
   },
-  /**
-   * canvas
-   */
-  // 下载背景图
-  getShareBgImg(banner) {
-    let that = this;
-    wx.downloadFile({
-      url: banner,
-      success: res => {
-        console.log('banner',res)
-        if (res.statusCode === 200) {
-          let shareBgImg = res.tempFilePath;
-          // that.getCodeImg(shareBgImg)
-          this.setData({ ['postConfig.shareBgImg']: shareBgImg })
-        } else {
-          wx.showToast({
-            title: '下载失败！',
-            success: () => {
-              let shareBgImg = ''
-              // that.getCodeImg(shareBgImg)
-              this.setData({ ['postConfig.shareBgImg']: shareBgImg })
-            }
-          })
-        }
-      },
-      fail: res => {
-        console.log(res)
-      }
-    })
-  },
-  //下载二维码
-  getCodeImg(qrcode, count = 1, timer = null) {
-    let that = this;
-    wx.downloadFile({
-      url: qrcode,
-      success: res => {
-        if (res.statusCode === 200) {
-          let codeImg = res.tempFilePath;
-          console.log('getCodeImg', codeImg)
-          // that.showCanvas(shareBgImg, codeImg)
-          this.setData({ ['postConfig.codeImg']: codeImg })
-        } else {
-          wx.showToast({
-            title: '下载失败！',
-            success: () => {
-              let codeImg = ''
-              // that.showCanvas(shareBgImg, codeImg)
-              this.setData({ ['postConfig.codeImg']: codeImg })
 
-            }
-          })
-        }
-      },
-      complete: res => {
-        console.log(res)
-        if (res.errMsg == 'downloadFile:fail Error: read ECONNRESET') {
-          if (count == 10) {
-            clearTimeout(timer)
-          } else {
-            timer = setTimeout(() => {
-              count++
-              this.getCodeImg(qrcode, count, timer)
-            }, 1000)
-          }
-        }
-      }
-    })
-  },
+// --------------------------------------------------------------------
   /**
-   * 2.创建画布
+   * @canvas
+   * getShareBgImg,getCodeImg,showCanvas,savePostLocation
+   * 该显示画布功能暂时关闭
    */
-  showCanvas(shareBgImg, codeImg) {
-    wx.showLoading({ title: '海报生成中...' })
-    let that = this;
-    const ctx = wx.createCanvasContext('myCanvas'); //创建画布
-    wx.createSelectorQuery().select('#canvas-container').boundingClientRect(function (rect) {
-      console.log(rect)
-      var height = rect.height;
-      var width = rect.width;
-      ctx.setFillStyle("#fff")
-      ctx.fillRect(0, 0, width, height)
-      //素材展示 所有的比例按照 750宽和 670高算
-      if (shareBgImg) {
-        ctx.drawImage(shareBgImg, 0, 0, width, height)
-      }
-      if (codeImg) {
-        ctx.drawImage(codeImg, width / 22.7, height / 1.27, width / 7, width / 7)
-      }
-      //text
-      ctx.setFontSize(height / 28)
-      ctx.setTextAlign('left')
-      ctx.setFillStyle("#fff")
-      ctx.fillText('点击并保存分享', width / 22.7 + width / 7 + width / 47, height / 1.27 + width / 7 / 2 + height / 28 / 2)
-      // ctx.stroke()
-    }).exec()
-    setTimeout(() => {
-      ctx.draw()
-      wx.hideLoading()
-    }, 100)
-  },
-  /**
-   * 3.保存本地
-   */
-  savePostLocation() {
-    var that = this;
-    wx.showLoading({ title: '正在保存', mask: true, })
-    setTimeout(() => {
-      wx.canvasToTempFilePath({
-        canvasId: 'myCanvas',
-        fileType: 'jpg',
-        success: function (res) {
-          wx.hideLoading();
-          var tempFilePath = res.tempFilePath;
-          wx.saveImageToPhotosAlbum({
-            filePath: tempFilePath,
-            success: function (res) {
-              console.log(res)
-              wx.showModal({
-                title: '提示',
-                content: '您的推广海报已存入手机相册，赶快分享给好友吧',
-                showCancel: false,
-              })
-            },
-            fail: function (err) {
-              console.log(err)
-              // 防止用户禁止了授权,这须手动调起权限了
-              if (err.errMsg === "saveImageToPhotosAlbum:fail:auth denied" || err.errMsg === "saveImageToPhotosAlbum:fail auth deny" || err.errMsg === "saveImageToPhotosAlbum:fail authorize no response") {
-                // 这边微信做过调整，必须要在按钮中触发，因此需要在弹框回调中进行调用
-                wx.showModal({
-                  title: '提示',
-                  content: '需要您授权保存相册',
-                  showCancel: false,
-                  success: modalSuccess => {
-                    wx.openSetting({
-                      success(settingdata) {
-                        console.log("settingdata", settingdata)
-                        if (settingdata.authSetting['scope.writePhotosAlbum']) {
-                          wx.showModal({
-                            title: '提示',
-                            content: '获取权限成功,再次确认即可保存',
-                            showCancel: false,
-                          })
-                        } else {
-                          wx.showModal({
-                            title: '提示',
-                            content: '获取权限失败，将无法保存到相册哦~',
-                            showCancel: false,
-                          })
-                        }
-                      },
-                      fail(failData) {
-                        console.log("failData", failData)
-                      },
-                      complete(finishData) {
-                        console.log("finishData", finishData)
-                      }
-                    })
-                  }
-                })
-              }
-            }
-          })
-        }
-      })
-    })
-  },
+  // 长摁识别
+  // distinguishImg() {
+  //   this.setData({
+  //     showShareState: true
+  //   }, () => {
+  //     this.showCanvas(this.data.postConfig.shareBgImg, this.data.postConfig.codeImg)
+  //   })
+  // },
+  // 关闭分享
+  // onCloseShareImg() {
+  //   this.setData({
+  //     showShareState: false
+  //   })
+  // },
+  // //关闭海报
+  // onCloseShareImg() {
+  //   this.setData({
+  //     showShareState: false
+  //   })
+  // },
+  // 下载背景图
+  // getShareBgImg(banner) {
+  //   let that = this;
+  //   wx.downloadFile({
+  //     url: banner,
+  //     success: res => {
+  //       console.log('banner',res)
+  //       if (res.statusCode === 200) {
+  //         let shareBgImg = res.tempFilePath;
+  //         // that.getCodeImg(shareBgImg)
+  //         this.setData({ ['postConfig.shareBgImg']: shareBgImg })
+  //       } else {
+  //         wx.showToast({
+  //           title: '下载失败！',
+  //           success: () => {
+  //             let shareBgImg = ''
+  //             // that.getCodeImg(shareBgImg)
+  //             this.setData({ ['postConfig.shareBgImg']: shareBgImg })
+  //           }
+  //         })
+  //       }
+  //     },
+  //     fail: res => {
+  //       console.log(res)
+  //     }
+  //   })
+  // },
+  // //下载二维码
+  // getCodeImg(qrcode, count = 1, timer = null) {
+  //   let that = this;
+  //   wx.downloadFile({
+  //     url: qrcode,
+  //     success: res => {
+  //       if (res.statusCode === 200) {
+  //         let codeImg = res.tempFilePath;
+  //         console.log('getCodeImg', codeImg)
+  //         // that.showCanvas(shareBgImg, codeImg)
+  //         this.setData({ ['postConfig.codeImg']: codeImg })
+  //       } else {
+  //         wx.showToast({
+  //           title: '下载失败！',
+  //           success: () => {
+  //             let codeImg = ''
+  //             // that.showCanvas(shareBgImg, codeImg)
+  //             this.setData({ ['postConfig.codeImg']: codeImg })
+
+  //           }
+  //         })
+  //       }
+  //     },
+  //     complete: res => {
+  //       console.log(res)
+  //       if (res.errMsg == 'downloadFile:fail Error: read ECONNRESET') {
+  //         if (count == 10) {
+  //           clearTimeout(timer)
+  //         } else {
+  //           timer = setTimeout(() => {
+  //             count++
+  //             this.getCodeImg(qrcode, count, timer)
+  //           }, 1000)
+  //         }
+  //       }
+  //     }
+  //   })
+  // },
+  // /**
+  //  * 2.创建画布
+  //  */
+  // showCanvas(shareBgImg, codeImg) {
+  //   wx.showLoading({ title: '海报生成中...' })
+  //   let that = this;
+  //   const ctx = wx.createCanvasContext('myCanvas'); //创建画布
+  //   wx.createSelectorQuery().select('#canvas-container').boundingClientRect(function (rect) {
+  //     console.log(rect)
+  //     var height = rect.height;
+  //     var width = rect.width;
+  //     ctx.setFillStyle("#fff")
+  //     ctx.fillRect(0, 0, width, height)
+  //     //素材展示 所有的比例按照 750宽和 670高算
+  //     if (shareBgImg) {
+  //       ctx.drawImage(shareBgImg, 0, 0, width, height)
+  //     }
+  //     if (codeImg) {
+  //       ctx.drawImage(codeImg, width / 22.7, height / 1.27, width / 7, width / 7)
+  //     }
+  //     //text
+  //     ctx.setFontSize(height / 28)
+  //     ctx.setTextAlign('left')
+  //     ctx.setFillStyle("#fff")
+  //     ctx.fillText('点击并保存分享', width / 22.7 + width / 7 + width / 47, height / 1.27 + width / 7 / 2 + height / 28 / 2)
+  //     // ctx.stroke()
+  //   }).exec()
+  //   setTimeout(() => {
+  //     ctx.draw()
+  //     wx.hideLoading()
+  //   }, 100)
+  // },
+  // /**
+  //  * 3.保存本地
+  //  */
+  // savePostLocation() {
+  //   var that = this;
+  //   wx.showLoading({ title: '正在保存', mask: true, })
+  //   setTimeout(() => {
+  //     wx.canvasToTempFilePath({
+  //       canvasId: 'myCanvas',
+  //       fileType: 'jpg',
+  //       success: function (res) {
+  //         wx.hideLoading();
+  //         var tempFilePath = res.tempFilePath;
+  //         wx.saveImageToPhotosAlbum({
+  //           filePath: tempFilePath,
+  //           success: function (res) {
+  //             console.log(res)
+  //             wx.showModal({
+  //               title: '提示',
+  //               content: '您的推广海报已存入手机相册，赶快分享给好友吧',
+  //               showCancel: false,
+  //             })
+  //           },
+  //           fail: function (err) {
+  //             console.log(err)
+  //             // 防止用户禁止了授权,这须手动调起权限了
+  //             if (err.errMsg === "saveImageToPhotosAlbum:fail:auth denied" || err.errMsg === "saveImageToPhotosAlbum:fail auth deny" || err.errMsg === "saveImageToPhotosAlbum:fail authorize no response") {
+  //               // 这边微信做过调整，必须要在按钮中触发，因此需要在弹框回调中进行调用
+  //               wx.showModal({
+  //                 title: '提示',
+  //                 content: '需要您授权保存相册',
+  //                 showCancel: false,
+  //                 success: modalSuccess => {
+  //                   wx.openSetting({
+  //                     success(settingdata) {
+  //                       console.log("settingdata", settingdata)
+  //                       if (settingdata.authSetting['scope.writePhotosAlbum']) {
+  //                         wx.showModal({
+  //                           title: '提示',
+  //                           content: '获取权限成功,再次确认即可保存',
+  //                           showCancel: false,
+  //                         })
+  //                       } else {
+  //                         wx.showModal({
+  //                           title: '提示',
+  //                           content: '获取权限失败，将无法保存到相册哦~',
+  //                           showCancel: false,
+  //                         })
+  //                       }
+  //                     },
+  //                     fail(failData) {
+  //                       console.log("failData", failData)
+  //                     },
+  //                     complete(finishData) {
+  //                       console.log("finishData", finishData)
+  //                     }
+  //                   })
+  //                 }
+  //               })
+  //             }
+  //           }
+  //         })
+  //       }
+  //     })
+  //   })
+  // },
 })
