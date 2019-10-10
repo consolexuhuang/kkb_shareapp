2e8f6c4bd430ac79d03688159e0f4c9447009707
consolexuhuang <1758050394@qq.com>
2019-10-10 18:42:22 +0800

    更新落地页

--- app.js
+++ app.js
@@ -85,21 +85,21 @@ App({
     scene:'',
     shareTicket_option:'',
 
-    //测试公众号地址
-    copyLinkGZH: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzUxNDc0OTcxMg==&scene=110#wechat_redirect',
-    //正式公众号地址
-    // copyLinkGZH: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU0OTcyNDU4Ng==&scene=110#wechat_redirect',
+    // //测试公众号地址
+    // copyLinkGZH: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzUxNDc0OTcxMg==&scene=110#wechat_redirect',
+    // 正式公众号地址
+    copyLinkGZH: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU0OTcyNDU4Ng==&scene=110#wechat_redirect',
 
-    JumpAppId: {                    //测试
-      appid: 'wx6b00bfc932f22210',
-      // envVersion: 'trial' //体验版
-      envVersion: 'release' //正式版
-    },
-    // JumpAppId: {                   //正式
-    //   appid: 'wx29946485f206d315',
+    // JumpAppId: {                    //测试
+    //   appid: 'wx6b00bfc932f22210',
     //   // envVersion: 'trial' //体验版
     //   envVersion: 'release' //正式版
-    // }, 
+    // },
+    JumpAppId: {                   //正式
+      appid: 'wx29946485f206d315',
+      // envVersion: 'trial' //体验版
+      envVersion: 'release' //正式版
+    }, 
   },
   //校验是否通过登陆
   passIsLogin() {
