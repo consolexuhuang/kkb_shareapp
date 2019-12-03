80a2fe366f4bc2773f503d10d0b0e5be61ac6ebe
consolexuhuang <1758050394@qq.com>
2019-12-03 10:23:08 +0800

    更新配置

--- app.js
+++ app.js
@@ -86,9 +86,9 @@ App({
     shareTicket_option:'',
 
     //测试公众号地址
-    copyLinkGZH: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzUxNDc0OTcxMg==&scene=110#wechat_redirect',
+    // copyLinkGZH: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzUxNDc0OTcxMg==&scene=110#wechat_redirect',
     // 正式公众号地址
-    // copyLinkGZH: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU0OTcyNDU4Ng==&scene=110#wechat_redirect',
+    copyLinkGZH: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU0OTcyNDU4Ng==&scene=110#wechat_redirect',
 
     // JumpAppId: {                    //测试
     //   appid: 'wx6b00bfc932f22210',
